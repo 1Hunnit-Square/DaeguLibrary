16528d13bedd4124c07ac09afce5e6eadf7a1836
Park-JiMinn <min.projects0222@gmail.com>
2025-05-09 17:02:58 +0900

    feat: ProgramInfo 엔티티, DTO, Repository 및 테스트 코드 추가

---
+++ dglib_api/src/main/java/com/dglib/repository/ProgramInfoRepository.java
@@ -0,0 +1,9 @@
+package com.dglib.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dglib.entity.ProgramInfo;
+
+public interface ProgramInfoRepository extends JpaRepository<ProgramInfo, Long>{
+
+}

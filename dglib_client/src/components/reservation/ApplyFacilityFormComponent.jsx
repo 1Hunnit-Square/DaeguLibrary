e43ec3cfe9a0d63b4ab4a190c33f0051d34eedf4
Hyojin Lim <ljinjin0803@gmail.com>
2025-05-23 18:06:08 +0900

    신청내역 페이지(달력, 신청서 폼) create 예정

--- dglib_client/src/components/reservation/ApplyFacilityFormComponent.jsx
+++ dglib_client/src/components/reservation/ApplyFacilityFormComponent.jsx
@@ -1,13 +1,7 @@
 const ApplyFacilityFormComponent = () => {
 
     return (
-        <div className="max-w-3xl mx-auto px-6 py-12 bg-white rounded shadow">
-            <h2 className="text-2xl font-bold mb-6">신청접수</h2>
-            {error && <p className="text-red-500 mb-4">{error}</p>}
-            <form onSubmit={handleSubmit} className="space-y-6 text-sm">
-                
-            </form>
-        </div>
+        <div>신청 접수 폼</div>
     )
 
 }

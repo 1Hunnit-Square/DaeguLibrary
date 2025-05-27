45f9712e815bf5a6ea683196c531c0acd1dade36
baek <bsm100100@gmail.com>
2025-05-27 14:48:37 +0900

    Merge remote-tracking branch 'origin/books' into member
    # Conflicts:
    #	dglib_api/src/main/java/com/dglib/controller/member/MemberController.java
    #	dglib_api/src/main/java/com/dglib/repository/member/MemberRepository.java
    #	dglib_api/src/main/java/com/dglib/security/jwt/JwtFilter.java
    #	dglib_api/src/main/java/com/dglib/security/jwt/JwtProvider.java
    #	dglib_api/src/main/java/com/dglib/service/member/MemberService.java
    #	dglib_api/src/main/java/com/dglib/service/member/MemberServiceImpl.java
    #	dglib_client/package-lock.json
    #	dglib_client/package.json
    #	dglib_client/src/api/memberApi.jsx
    #	dglib_client/src/components/common/SelectComponent.jsx
    #	dglib_client/src/components/member/LoginComponent.jsx
    #	dglib_client/src/hooks/usePagination.jsx
    #	dglib_client/src/pages/AdminPage.jsx
    #	dglib_client/src/pages/MyLibraryPage.jsx
    #	dglib_client/src/routers/myLibraryRouter.jsx

--- dglib_client/src/api/memberApi.jsx
+++ dglib_client/src/api/memberApi.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { API_SERVER_HOST, API_ENDPOINTS } from './config';
-import axiosClient from '../util/AxiosClient';
+import axiosClient from '../util/axiosClient';
 
 const prefix = `${API_SERVER_HOST}${API_ENDPOINTS.member}`;
 

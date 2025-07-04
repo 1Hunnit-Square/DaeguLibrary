3b0bdc954b7663d14abaccd73355594c8a674433
bsm100100 <bsm100100@gmail.com>
2025-07-04 17:39:35 +0900

    도서관 css 살짝 수정 이제 과연!

--- dglib_client/src/components/main/ClosedInfoComponent.jsx
+++ dglib_client/src/components/main/ClosedInfoComponent.jsx
@@ -41,8 +41,8 @@ const ClosedInfoComponent = () => {
         <div className="w-full h-full flex justify-center overflow-hidden">
             <div className="w-full h-full flex flex-col items-center justify-center text-center">
                 {/* 이용시간 */}
-                <div>
-                    <div className="font-semibold text-xs sm:text-sm text-gray-800 mb-1">이용시간</div>
+                <div className="py-5 h-full">
+                    <div className="font-semibold text-xs sm:text-sm text-gray-800 mb-3">이용시간</div>
                     <div className="text-xs sm:text-sm text-gray-700 space-y-0.5">
                         <div className="flex justify-center gap-2 sm:gap-4 whitespace-nowrap">
                             <span className="font-semibold flex items-center">
@@ -62,11 +62,11 @@ const ClosedInfoComponent = () => {
                 </div>
 
                 {/* 구분선 */}
-                <div className="w-full max-w-sm border-t border-gray-300 sm:my-1 lg:my-5" />
+                <div className="w-full max-w-sm border-t border-gray-300" />
 
                 {/* 휴관일 */}
-                <div>
-                    <div className="text-xs sm:text-sm font-bold text-gray-800 mb-1">휴관일</div>
+                <div className="py-5 h-full">
+                    <div className="text-xs sm:text-sm font-bold text-gray-800 mb-3">휴관일</div>
                     <div className="flex items-center justify-center gap-2 text-xs sm:text-sm text-gray-800 mb-1">
                         <button onClick={handlePrevMonth} className="font-bold text-green-600 hover:text-green-700 cursor-pointer">〈</button>
                         <span className="whitespace-nowrap">{`${year}년 ${month}월`}</span>
